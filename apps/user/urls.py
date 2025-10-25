1627227b7f6e4761061532e0356d116bb3e16e7c
nuell100 <plab100.du@proton.me>
2025-10-25 10:09:50 +0500

    add djoser

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     # path('singup/', SignUpAPIView.as_view(), name='signup_api_view'),
     # path('login/', LoginAPIView.as_view(), name='login_api_view'),
     # path('change-passwd/', ChangePasswordView.as_view(), name='change_passwd_api_view'),
-    re_path(r'^auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
 ]
